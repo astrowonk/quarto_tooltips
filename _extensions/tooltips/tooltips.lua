2ecf0c7c9d47c93c476b009b869971e2d89a4300
Marcos Huerta <marcos@marcoshuerta.com>
2024-12-08 08:52:23 -0500

    bump version

--- _extensions/tooltips/tooltips.lua
+++ _extensions/tooltips/tooltips.lua
@@ -1,7 +1,7 @@
 local function ensure_html_deps()
   quarto.doc.add_html_dependency({
     name = 'load_tooltips',
-    version = '1.0.0',
+    version = '1.0.1',
     scripts = { { path = "load_tooltips.js", afterBody = true } }
   })
 end
